bf1be187ca1ef1cb2787372663d2775930880f07
Francesco Favero <favero.francesco@gmail.com>
2011-07-21 14:21:10 +0200

    avoid lookup on list error

--- Platforms/utils.py
+++ Platforms/utils.py
@@ -220,7 +220,8 @@ def get_geo_annot_split(acc,fields):
                      accessions = line[1:]
                      tmp_dict[idref] = {}
                      for item in annot_lookup.keys():
-                        tmp_dict[idref][item] = accessions[annot_lookup[item]]
+                        if len(accessions) >= annot_lookup[item]:
+                           tmp_dict[idref][item] = accessions[annot_lookup[item]]
                if switch == 'OFF':
                   if find_start.match(line):
                      switch = 'ON'
