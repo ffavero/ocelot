271bec940acf34904d977b7c41a0bdce4007e1be
Francesco Favero <favero.francesco@gmail.com>
2011-08-16 17:40:44 +0200

    Add dowload image as in the PHP version

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response
 from xml.etree import ElementTree as ET
@@ -7,6 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
+from settings import ROOT_PATH
+import os
 
 @login_required
 def admin(request):
@@ -34,15 +37,32 @@ def help(request):
 
 def export_csv(request):
    '''
-   Create the HttpResponse object with the appropriate CSV header.
+   Create the HttpResponse object with the appropriate TXT header.
    '''
    data = None
    if request.method == 'POST':
       data = request.POST
    response = HttpResponse(data['csv_download'], content_type='text/plain')
-   response['Content-Disposition'] = 'attachment; filename='+data['file_name']+'.csv'
+   response['Content-Disposition'] = 'attachment; filename='+data['file_name']+'.txt'
    return response
 
+def export_png(request):
+   '''
+   Create the HttpResponse object with the appropriate png header.
+   '''
+   data = None
+   if request.method == 'POST':
+      data = request.POST
+   path_name = ROOT_PATH + data['img_name']
+   img_name  =  path_name.split('/')[-1]
+   if os.path.isfile(path_name):
+      with open(path_name, "rb") as img:
+         tmp = img.read()
+      response = HttpResponse(tmp, content_type='image/png')
+      response['Content-Disposition'] = 'attachment; filename=' +img_name
+      return response
+   else:
+      return HttpResponse("Not Found",mimetype="text/plain")
 
 def Datasets2XML(request):
    '''
